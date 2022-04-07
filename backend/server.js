00467cfbfc6055bc402090e6bcbc785cce811366
saahil0826 <snawalkha086@gmail.com>
2022-04-07 18:38:44 +0530

    installed express-async-handler

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv").config()
+const { errorHandler } = require('./middleware/errorMiddleware')
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json()); //body parser
@@ -8,5 +9,7 @@ app.use(express.urlencoded({extended: false}));
 
 app.use("/api/goals", require('./routes/goalRoutes'));
 //mount the router
+
+app.use(errorHandler)
 
 app.listen(PORT, console.log(`now listening on port ${PORT}`));
