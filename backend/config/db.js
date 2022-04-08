09c6b3243efcf0e617608dc084056901f6214903
saahil0826 <snawalkha086@gmail.com>
2022-04-08 18:01:50 +0530

    REST API for goals

---
+++ backend/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
+//this will be imported the main server.js file
